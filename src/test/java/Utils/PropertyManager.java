176730aeb77302509d41da5376896fc088af3158
817870 <svce.rahula@gmail.com>
2020-06-05 06:27:39 +0530

    Rest Assured library automation framework to automate Google Fit API, which makes use of BDD approach of automation too. Enums is made use of here. Also made use of TestContext to share data between different stepdefs of different Scenarios.

---
+++ src/test/java/Utils/PropertyManager.java
@@ -0,0 +1,36 @@
+package Utils;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+public class PropertyManager {
+
+    public static Properties configProp =  new Properties();
+    private static String bearerToken;
+    private static String dataSourcesPath;
+    private static String googleFitnessApi;
+
+    public static Properties loadProperties() throws IOException {
+        FileInputStream fis = new FileInputStream(System.getProperty("user.dir")
+                +"\\src\\test\\java\\resources\\config.properties");
+        configProp.load(fis);
+        bearerToken = configProp.getProperty("bearerToken");
+        dataSourcesPath = configProp.getProperty("dataSourcesPath");
+        googleFitnessApi = configProp.getProperty("googleFitnessApi");
+        return configProp;
+    }
+
+    public String getBearerToken() {
+        return bearerToken;
+    }
+
+    public String getDataSourcesPath() {
+        return dataSourcesPath;
+    }
+
+    public String getGoogleFitnessApi() {
+        return googleFitnessApi;
+    }
+}
---
+++ src/test/java/Utils/TestContext.java
@@ -0,0 +1,21 @@
+package Utils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TestContext {
+
+    public static ThreadLocal<HashMap<Object,Object>> testData;
+
+    public  TestContext() {
+         testData = ThreadLocal.withInitial(HashMap::new);
+    }
+
+    public void setContext(Object key, Object value) {
+         testData.get().put(key,value);
+    }
+
+    public Object getContextValue(Object value) {
+        return testData.get().get(value);
+    }
+}
